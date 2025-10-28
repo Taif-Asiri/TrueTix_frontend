19c66a4cee4aa5bda35e6c138ba84a316bcd578e
Taif-Asiri <toti4438@gmail.com>
2025-10-28 20:31:43 +0300

    connected the App.jsx file with the MyTickets component using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventList from "./components/EventList";
-
+import MyTickets from "./components/MyTickets";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<EventList />} />
+        <Route path="/" element={<MyTickets />} />
       </Routes>
     </Router>
   );

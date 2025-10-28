b45cf512ea9b0f0d4a9511e1a59dc851544b9885
Taif-Asiri <toti4438@gmail.com>
2025-10-28 21:41:02 +0300

    Connected the App.jsx file with the ResellForm component using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventList from "./components/EventList";
 import MyTickets from "./components/MyTickets";
+import MyTickets from "./components/ResellForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<EventList />} />
         <Route path="/" element={<MyTickets />} />
+        <Route path="/" element={<ResellForm />} />
       </Routes>
     </Router>
   );

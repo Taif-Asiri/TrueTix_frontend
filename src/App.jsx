300d34a639112d32ed29768ee5a9a735be054d3a
Taif-Asiri <toti4438@gmail.com>
2025-10-30 12:30:35 +0300

    add tokens to urls , update settings.py JWT + REST FEAMEWORK

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventList from "./components/EventList";
-import EventDetails from "./components/EventDetails";
+import EventDetails from "./components/EventDetail";
 import MyTickets from "./components/MyTickets";
 import ResellForm from "./components/ResellForm";
 
--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,5 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
 });
 
+
 export default api;

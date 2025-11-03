6f0c9671d9d47eae70969f6fbfdef09d9367c871
Taif-Asiri <toti4438@gmail.com>
2025-11-03 11:16:57 +0300

    Protect the routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import SignupPage from "./components/SignupPage";
 import Verify from "./components/Verify";
 import HomePage from "./pages/Home";
 import Navbar from "./components/NavBar";
-
-
+import ProtectedRoute from "./components/ProtectedRoute"
+import UserProfile from "./components/UserProfile";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 
@@ -20,14 +20,44 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/events" element={<EventList />} />
-          <Route path="/tickets" element={<MyTickets />} />
           <Route path="/resell/:id" element={<ResellForm />} />
           <Route path="/events/:id" element={<EventDetail />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/navbar" element={<Navbar />} />
-
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/tickets"
+          element={
+            <ProtectedRoute>
+              <MyTickets />
+            </ProtectedRoute>
+          }
+          />
+        <Route
+          path="/resell"
+          element={
+            <ProtectedRoute>
+              <ResellForm />
+            </ProtectedRoute>
+          }
+          />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>  
